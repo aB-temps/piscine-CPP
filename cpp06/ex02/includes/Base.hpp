1fc05e377eccfc076918aacf54ced3a7769ce13f
aB-temps <187414947+aB-temps@users.noreply.github.com>
2026-03-10 15:04:56 +0100

    🔧 fix(cpp06-ex02): changed identify(&) implementation, without using ptr inside

--- cpp06/ex02/includes/Base.hpp
+++ cpp06/ex02/includes/Base.hpp
@@ -6,12 +6,15 @@
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 16:57:21 by abetemps          #+#    #+#             */
-/*   Updated: 2026/03/10 14:25:01 by abetemps         ###   ########.fr       */
+/*   Updated: 2026/03/10 14:50:20 by abetemps         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		__BASE_HPP__
 # define	__BASE_HPP__
+
+# define	DERIVED_COUNT	3
+# define	ELEMENTS		4
 
 class	Base
 {
--- cpp06/ex02/sources/main.cpp
+++ cpp06/ex02/sources/main.cpp
@@ -6,13 +6,14 @@
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 10:26:17 by abetemps          #+#    #+#             */
-/*   Updated: 2026/03/10 14:30:33 by abetemps         ###   ########.fr       */
+/*   Updated: 2026/03/10 15:03:22 by abetemps         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
+#include <exception>
 
 #include "factory.tpp"
 #include "A.hpp"
@@ -24,17 +25,22 @@ using	std::endl;
 
 Base	*generate(void)
 {
-	const t_builder	factory[3] =
+	const t_builder	factory[DERIVED_COUNT] =
 	{
 		newBase<A>,
 		newBase<B>,
 		newBase<C>
 	};
-	return (factory[std::rand() % 3]());
+	return (factory[std::rand() % DERIVED_COUNT]());
 }
 
 void	identify(Base *p)
 {
+	if (!p)
+	{
+		cout << "null ptr" << endl;
+		return ;
+	}
 	if (dynamic_cast<A *>(p))
 		cout << "A" << endl;
 	else if (dynamic_cast<B *>(p))
@@ -45,7 +51,30 @@ void	identify(Base *p)
 
 void	identify(Base &p)
 {
-	identify(&p);
+	try
+	{
+		Base &b = dynamic_cast<A &>(p);
+		(void) b;
+		cout << "A" << endl;
+		return ;
+	}
+	catch (const std::exception &e) {}
+	try
+	{
+		Base &b = dynamic_cast<B &>(p);
+		(void) b;
+		cout << "B" << endl;
+		return ;
+	}
+	catch (const std::exception &e) {}
+	try
+	{
+		Base &b = dynamic_cast<C &>(p);
+		(void) b;
+		cout << "C" << endl;
+		return ;
+	}
+	catch (const std::exception &e) {}
 }
 
 
@@ -53,19 +82,21 @@ int main(void)
 {
 	std::srand(time(0));
 
-	Base	*b[3] = {0};
+	Base	*b[ELEMENTS] = {0};
 
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < ELEMENTS; ++i)
 		b[i] = generate();
 
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < ELEMENTS; ++i)
 		identify(b[i]);
 
 	cout << endl;
 
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < ELEMENTS; ++i)
 		identify(*b[i]);
 
+	for (int i = 0; i < ELEMENTS; ++i)
+		delete b[i];
 
 	return (0);
 }
