ef363ef6d1673e6ed8e5bee8734d93e1da751c72
aB-temps <187414947+aB-temps@users.noreply.github.com>
2026-03-21 17:06:14 +0100

    ➕ add(cpp08-ex02): basic structure

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -1,50 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Span.hpp                                           :+:      :+:    :+:   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 16:57:21 by abetemps          #+#    #+#             */
-/*   Updated: 2026/03/21 17:01:14 by abetemps         ###   ########.fr       */
+/*   Updated: 2026/03/21 17:03:03 by abetemps         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		__SPAN_HPP__
-# define	__SPAN_HPP__
+#ifndef		__MUTANTSTACK_HPP__
+# define	__MUTANTSTACK_HPP__
 
-# include <iostream>
-# include <vector>
 
 // Class declaration ===========================================================
-class	Span
+class	MutantStack
 {
 	public:
-		Span(const unsigned int &capacity);
-		Span(const Span &copy);
-		~Span(void);
-
-		Span			&operator=(const Span &assign);
-		int				&operator[](const unsigned int &i);
-		const int		&operator[](const unsigned int &i) const;
-	            		
-		void			addNumber(const int &n);
-		void			addRange(const unsigned int &r);
-		int				longestSpan(void) const;
-		int				shortestSpan(void) const;
-
-		void			outOfRange(const unsigned int &i, const unsigned char mode) const;
-
-		unsigned int	getSize(void) const;
 
 	private:
-		std::vector<int>	_array;
-		unsigned int		_capacity;
-		
 
 };
 
 // Utils =======================================================================
-std::ostream	&operator<<(std::ostream &out, const Span &sp);
+std::ostream	&operator<<(std::ostream &out, const MutantStack &mst);
+
+# include "MutantStack.tpp"
 
 #endif
